6a539a3e0f1b17440aa64581961d134cd61ee62d
Mike Covington <mfcovington@gmail.com>
2015-11-19 20:21:51 -0800

    Incorporate LiveReload into 'gulp watch' for stylesheets and templates

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,39 @@
 
 var autoprefixer = require('gulp-autoprefixer');
 var gulp = require('gulp');
+var livereload = require('gulp-livereload');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 
+var appName = 'cms_lab_publications';
 var paths = {
-    css: 'cms_lab_publications/static/cms_lab_publications/css',
-    sass: 'cms_lab_publications/sass/app.scss',
-}
+    css: appName + '/static/' + appName + '/css',
+    sass: appName + '/sass/app.scss',
+    templates: '**/templates/**/*.html',
+};
 
 gulp.task('sass', function() {
-    gulp.src(paths.sass)
+    return gulp.src(paths.sass)
         .pipe(sourcemaps.init())
             .pipe(sass({ outputStyle: 'compressed' }))
             .pipe(autoprefixer())
         .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(paths.css));
+        .pipe(gulp.dest(paths.css))
+        .pipe(livereload());
 });
 
+function touchPy() {
+    gulp.src(appName + '/__init__.py')
+        .pipe(gulp.dest(appName));
+}
+
 gulp.task('watch', function() {
+    livereload.listen();
     gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.templates).on('change', function(file) {
+        touchPy();
+        livereload.changed(file);
+    });
 });
 
 gulp.task('default', ['sass', 'watch']);
